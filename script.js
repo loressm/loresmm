9b0e1da515d88c8af10c39d3feb9c74425eee762
loressm <lorerome90@virgilio.it>
2025-03-17 14:39:55 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Funzione per aprire/chiudere il menu
-function toggleMenu() {
-    const menu = document.getElementById("menu");
-    const hamburger = document.querySelector(".hamburger");
-
-    // Aggiungi o rimuovi la classe "open" per il menu e l'icona dell'hamburger
-    menu.classList.toggle("open");
-    hamburger.classList.toggle("open");
-}
